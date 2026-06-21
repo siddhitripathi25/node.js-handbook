6b233e0aa022e3fe4482f454897c97f713ef5788
Siddhi Tripathi <siddhitripathi104@gmail.com>
2026-06-21 12:09:07 +0530

    REPL

--- first.js
+++ first.js
@@ -1,4 +1,5 @@
 //FOR WRITING DATA TO LOCAL FILE
+//USE OF REQUIRE FUNCTION TO IMPORT FILE SYSTEM MODULE
 const fs = require('fs');
 fs.writeFile('output.txt', 'WritingFile', (err)=>{
     if (err) throw err;
